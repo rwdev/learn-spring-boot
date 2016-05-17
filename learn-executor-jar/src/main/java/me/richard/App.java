bd57646980386c6b1fd89a4b2050f1b0a57834d1
richard <wangli9li@gmail.com>
2016-05-17 17:55:15 +0800

    add learn-executor-jar

---
+++ learn-executor-jar/src/main/java/me/richard/App.java
@@ -0,0 +1,23 @@
+package me.richard;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Scheduled(fixedRate = 1000)
+	public void tick() {
+		System.out.println("current time:" + LocalDateTime.now());
+	}
+
+}
